f218518b7f44f523c6e2e0cddde1c61e82b831d5
Ivan Shalin <shalinivan21@gmail.com>
2024-10-30 23:58:18 -0400

    Updated sql queries o include view

--- database/expenses.sql
+++ database/expenses.sql
@@ -21,5 +21,31 @@ CREATE TABLE expenses (
     category_id INTEGER NOT NULL,
     amount FLOAT NOT NULL,
     date TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    description TEXT
+    description TEXT,
+    FOREIGN KEY (user_id) REFERENCES users(user_id) ON DELETE CASCADE,
+    FOREIGN KEY (category_id) REFERENCES categories(category_id) ON DELETE CASCADE
 );
+
+CREATE VIEW IF NOT EXISTS user_expenses AS
+SELECT users.username, categories.category_name, expenses.amount, expenses.date, expenses.description
+FROM expenses
+JOIN users ON expenses.user_id = users.user_id
+JOIN categories ON expenses.category_id = categories.category_id;
+
+-- to check the total expenses by each user
+SELECT user_id, SUM(amount) AS total_spent
+FROM expenses
+GROUP BY user_id;
+
+-- To Check expenses by category
+SELECT category_id, SUM(amount) AS total_spent
+FROM expenses
+GROUP BY category_id;
+
+-- Display
+SELECT * FROM users;
+SELECT * FROM categories;
+SELECT * FROM expenses;
+
+
+
